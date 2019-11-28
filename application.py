f209ea71a53a74e88579ba0c8f3ea4f12efeb99f
Karina Halevy <karina.halevy@gmail.com>
2019-11-28 19:29:51 +0000

    prelim functions

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from cs50 import SQL
+import sqlite3
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -8,7 +8,6 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 from helpers import apology, login_required, lookup, usd, pass_strong
-# personal touch: checking password strength
 
 # Configure application
 app = Flask(__name__)
@@ -35,7 +34,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+connection = sqlite3.connect("tracker.db")
+db = connection.cursor()
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
@@ -45,92 +45,22 @@ if not os.environ.get("API_KEY"):
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
-    balance = db.execute("SELECT cash FROM users WHERE username=:username", username=session['user_id'])[0]['cash']
-    owned = db.execute("SELECT symbol, SUM(shares) FROM bought WHERE user=:username GROUP BY symbol", username=session['user_id'])
-    grand_total = 0
-    stocks = []
-    total = 0
-    for stock in owned:
-        symbol = stock['symbol']
-        if not symbol:
-            break
-        info = lookup(symbol)
-        shares = stock['SUM(shares)']
-        if shares == 0:
-            break
-        price = info['price']
-        name = info['name']
-        total = shares*price
-        grand_total += total
-        stocks.append([symbol, name, shares, usd(price), usd(total)])
-    grand_total += balance
-    return render_template("index.html", cash=usd(balance), stocks=stocks, grand_total=usd(grand_total))
+    return apology("to do")
 
-
-@app.route("/buy", methods=["GET", "POST"])
+@app.route("/goal1", methods=["GET", "POST"])
 @login_required
-def buy():
+def goal1():
     if request.method == "GET":
-        return render_template("buy.html")
-    symbol = request.form.get("symbol").upper()
-    info = lookup(symbol)
-    if not info:
-        return apology("Invalid symbol.")
-    shares = request.form.get("shares")
-    if not shares:
-        return apology("Please enter a positive number of shares.")
-    acceptable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for character in shares:
-        if character not in acceptable:
-            return apology("Input must be a positive integer.")
-    shares = int(shares)
-    if shares <= 0:
-        return apology("Input must be a positive integer.")
-    price = info['price']
-    name = info['name']
-    total = price*shares
-    balance = db.execute("SELECT cash FROM users WHERE username=:username", username=session['user_id'])[0]['cash']
-    if total > balance:
-        return apology("You don't have enough money.")
-    time = datetime.now()
-    hist = db.execute("SELECT symbol, shares FROM bought WHERE symbol=:symbol AND user=:username",
-                      symbol=symbol, username=session['user_id'])
-    if not hist:
-        db.execute("INSERT INTO bought (user, symbol, shares, price, time) VALUES (:user, :symbol, :shares, :price, :time)",
-                   user=session['user_id'], symbol=symbol, shares=shares, price=price, time=time)
-    else:
-        hist = hist[0]
-        exists = hist['symbol']
-        if not exists:
-            db.execute("INSERT INTO bought (user, symbol, shares, price, time) VALUES (:user, :symbol, :shares, :price, :time)",
-                       user=session['user_id'], symbol=symbol, shares=shares, price=price, time=time)
-        else:
-            new_shares = hist['shares'] + shares
-            db.execute("UPDATE bought SET shares = :shares WHERE user=:user AND symbol=:symbol",
-                       shares=new_shares, user=session['user_id'], symbol=symbol)
-    db.execute("INSERT INTO history (user, type, shares, symbol, price, time) VALUES (:user, 'buy', :shares, :symbol, :price, :time)",
-               user=session['user_id'], shares=shares, symbol=symbol, price=price, time=time)
-    db.execute("UPDATE users SET cash = :cash WHERE username=:username", cash=balance - total, username=session['user_id'])
+        return apology("to do")
     return redirect("/")
 
 
-@app.route("/history")
+@app.route("/goal2", methods=["GET", "POST"])
 @login_required
-def history():
-    """Show history of transactions"""
-    data = db.execute("SELECT * FROM history WHERE user=:username", username=session['user_id'])
-    stocks = []
-    if data != []:
-        for item in data:
-            symbol = item['symbol']
-            price = item['price']
-            trans_type = item['type']
-            shares = item['shares']
-            time = item['time']
-            name = lookup(symbol)['name']
-            stocks.append([symbol, name, shares, usd(price), trans_type, time])
-    return render_template("history.html", stocks=stocks)
+def goal2():
+    if request.method == "GET":
+        return apology("to do")
+    return apology("to do")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -181,21 +111,12 @@ def logout():
     return redirect("/")
 
 
-@app.route("/quote", methods=["GET", "POST"])
+@app.route("/goal3", methods=["GET", "POST"])
 @login_required
-def quote():
+def goal3():
     if request.method == "GET":
-        return render_template("quote.html")
-    symbol = request.form.get("symbol")
-    try:
-        info = lookup(symbol)
-    except:
-        return apology("Invalid symbol.")
-    if not info:
-        return apology("Invalid symbol.")
-    amount = info['price']
-    company = info['name'] + " (" + info['symbol'] + ")"
-    return render_template("quoted.html", name=company, amount=amount)
+        return apology("to do")
+    return apology("to do")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -234,71 +155,13 @@ def register():
     return redirect("/")
 
 
-@app.route("/sell", methods=["GET", "POST"])
+@app.route("/enter", methods=["GET", "POST"])
 @login_required
-def sell():
+def enter():
     """Sell shares of stock"""
     # see what you can sell
-    owned = db.execute("SELECT symbol, SUM(shares) FROM bought WHERE user=:username GROUP BY symbol", username=session['user_id'])
-    symbols = []
-    for stock in owned:
-        symbol = stock['symbol']
-        if not symbol:
-            break
-        if stock['SUM(shares)'] != 0:
-            symbols.append(symbol)
     if request.method == "GET":
-        return render_template("sell.html", symbols=symbols)
-    # otherwise, do this stuff for a post request
-    symbol = request.form.get("symbol")
-    shares = request.form.get("shares")
-    if not shares:
-        return apology("Please input a positive number of shares.")
-    acceptable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for character in shares:
-        if character not in acceptable:
-            return apology("Input must be a positive integer.")
-    shares = int(shares)
-    if shares <= 0:
-        return apology("Input must be a positive integer.")
-    to_sell = ""
-    # can't sell more than you own
-    for stock in owned:
-        if stock['symbol'] == symbol:
-            if stock['SUM(shares)'] < shares:
-                return apology("You don't own that many shares.")
-            to_sell = stock['symbol']
-    if to_sell == "":
-        return apology("You don't own that stock.")
-    time = datetime.now()
-    info = lookup(symbol)
-    price = info['price']
-    total = price*shares
-    # update cash balance
-    balance = db.execute("SELECT cash FROM users WHERE username=:username", username=session['user_id'])[0]['cash']
-    # update the database differently depending on if you've sold or bought the stock in the past
-    hist = db.execute("SELECT symbol, shares FROM bought WHERE symbol=:symbol AND user=:username",
-                      symbol=symbol, username=session['user_id'])[0]
-    sellhist = db.execute("SELECT symbol, shares FROM sold WHERE symbol=:symbol AND user=:username",
-                          symbol=symbol, username=session['user_id'])
-    if not sellhist:
-        db.execute("INSERT INTO sold (user, symbol, shares, price, time) VALUES (:user, :symbol, :shares, :price, :time)",
-                   user=session['user_id'], symbol=to_sell, shares=shares, price=price, time=time)
-    else:
-        exists = sellhist[0]['symbol']
-        if not exists:
-            db.execute("INSERT INTO sold (user, symbol, shares, price, time) VALUES (:user, :symbol, :shares, :price, :time)",
-                       user=session['user_id'], symbol=to_sell, shares=shares, price=price, time=time)
-        else:
-            sold_shares = sellhist[0]['shares'] + shares
-            db.execute("UPDATE sold SET shares = :shares WHERE user=:user AND symbol=:symbol",
-                       shares=sold_shares, user=session['user_id'], symbol=to_sell)
-    new_shares = hist['shares'] - shares
-    db.execute("UPDATE bought SET shares = :shares WHERE user=:user AND symbol=:symbol",
-               shares=new_shares, user=session['user_id'], symbol=to_sell)
-    db.execute("INSERT INTO history (user, type, symbol, shares, price, time) VALUES (:user, 'sell', :symbol, :shares, :price, :time)",
-               user=session['user_id'], symbol=symbol, shares=shares, price=price, time=time)
-    db.execute("UPDATE users SET cash = :cash WHERE username=:username", cash=balance + total, username=session['user_id'])
+        return apology("to do")
     return redirect("/")
 
 
