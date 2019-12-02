a83db417487b3ce618e577a650099fb23ac265d3
Karina Halevy <karina.halevy@gmail.com>
2019-12-02 20:27:22 +0000

    refactoring

--- application.py
+++ application.py
@@ -47,12 +47,33 @@ if not os.environ.get("API_KEY"):
 def index():
     return apology("to do")
 
-@app.route("/goal1", methods=["GET", "POST"])
+@app.route("/enter_data_1", methods=["GET", "POST"])
 @login_required
-def goal1():
+def enter_data_1():
     if request.method == "GET":
-        return apology("to do")
-    return redirect("/")
+        return render_template("enter_data_1.html")
+    else:
+        didIt = lookup(request.form.get("tracker"))
+
+        # if no answer
+        if didIt == None:
+            return apology("Must return an answer!")
+
+
+        # get date info
+        now = datetime.datetime.now()
+        y = now.year
+        m = now.month
+        d = now.day
+        if db.execute("SELECT username, year, month, day FROM binary_goals WHERE u = :u AND y = :y AND m = :m AND d = :d", u = session['user_id'], y = y, m = m, d = d) == None:
+            db.execute("INSERT INTO binary_goals (username, year, month, day) VALUES (:u, :y, :m, :d)",
+                u = session['user_id'], y = y, m = m, d = d)
+        else:
+            # what should we display? just override prev entry or??? can only view calendar history if we log it or no?
+            pass
+
+
+        return render_template("goal_1_month.html")
 
 
 @app.route("/goal2", methods=["GET", "POST"])
