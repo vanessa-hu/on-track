24171e71befdf7525572c194114d5d47ab13203e
Karina Halevy <karina.halevy@gmail.com>
2019-12-04 01:45:50 +0000

    calendar moved

--- calendar.py
+++ calendar.py
@@ -3,3 +3,6 @@ import datetime
 y = int(input("Input the year : "))
 m = int(input("Input the month : "))
 print(calendar.month(y, m))
+# need to get month you're looking at. get.request
+# need to get get day of week you're looking at.
+if
