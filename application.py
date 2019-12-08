e741af62dda0368a83fa35d035215402196316fd
Karina Halevy <karina.halevy@gmail.com>
2019-12-08 06:43:55 +0000

    postgre

--- application.py
+++ application.py
@@ -34,7 +34,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///tracker.db")
+db = SQL(get_postgre())
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
